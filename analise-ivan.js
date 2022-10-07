26720c11ac4965d829055721b9e2c37f21f02599
Dymas <dymassousa@gmail.com>
2022-10-07 12:15:32 -0300

    Lendo nomes de arquivo de um diretório

--- analise-ivan.js
+++ analise-ivan.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
 
 async function processLineByLine(filename) {
-  const fileStream = fs.createReadStream(filename);
+    const fileStream = fs.createReadStream(filename);
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
 
-  let errorsIds = '';
+    let errorsIds = '';
 
-  for await (const line of rl) {
-    //console.log(`Line from file: ${line}`);
-    if (line.includes("['radiologist_name', 'radiologist_crm_state', 'radiologist_crm_number']")) {
-        //console.log(`Linha: ${line.slice(11, 19)}`);
-        errorsIds += line.slice(11, 19) + ', ';
+    for await (const line of rl) {
+        if (line.includes("['radiologist_name', 'radiologist_crm_state', 'radiologist_crm_number']")) {
+            errorsIds += line.slice(11, 19) + ', ';
+        }
     }
-  }
-  
-  return (errorsIds.slice(0, -2));
+
+    return (errorsIds.slice(0, -2));
 }
 
-processLineByLine('log_error_20220909_181953.txt');
+
+async function readFilesFromDir(dirName) {
+    const directoryPath = path.join(__dirname, dirName);
+    await fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        return files;
+    });
+}
